dd3f40080b8b7a9a8c4de2085f9d9014720a440a
thecetinkaya <210905062@ogrenci.karatekin.edu.tr>
2023-09-01 14:27:15 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const link='http://www.google.com';
+  // const person = {name: 'yoshi', age:30};
+  return (
+    <Router>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/create">
+            <Create/>
+          </Route>
+
+          <Route path="/blogs/:id">
+            <BlogDetails/>
+          </Route>
+          <Route path="*">
+            <NotFound/>
+          </Route>
+
+        </Switch>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
